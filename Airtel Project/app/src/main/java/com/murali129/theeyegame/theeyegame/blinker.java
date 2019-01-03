a07436bbe8174d9c871d664ba7cb4f1012dc37bd
Anukriti12 <anu1999kriti@gmail.com>
2019-01-03 11:13:54 +0530

    Major changes: open and close probabilities

---
+++ Airtel Project/app/src/main/java/com/murali129/theeyegame/theeyegame/blinker.java
@@ -0,0 +1,11 @@
+package com.murali129.theeyegame.theeyegame;
+
+public class blinker {
+    static boolean blinked;
+    static float leftEyeprob;
+    static float rightEyeprob;
+    public blinker() {
+       leftEyeprob = -1;
+       rightEyeprob = -1;
+    }
+}
